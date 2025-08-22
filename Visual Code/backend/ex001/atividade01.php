33bf67a5e898ba933c5a379781aabce54137c744
Eukingzinn <thallesoares2@gmail.com>
2025-08-22 10:14:35 -0300

    atividades realizadas dia 22/08/25

---
+++ Visual Code/backend/ex001/atividade01.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu perfil</title>
+</head>
+<body>
+    <?php 
+    $meu_nome = "Thalles Eduardo Soares Miranda" ;
+    $minha_idade = 17 ;
+    
+    echo "Meu nome é $meu_nome e eu tenho $minha_idade anos." ;
+    ?>
+
+</body>
+</html>
---
+++ Visual Code/backend/ex001/atividade02.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inspetor de Tipos</title>
+</head>
+<body>
+    
+<?php 
+$string = "PHP" ;
+$int = 2025 ;
+$float = 9.5 ;
+$bool = true ;
+
+var_dump($string) ;
+var_dump($int) ;
+var_dump($float) ;
+var_dump($bool) ;
+?>
+
+</body>
+</html>
