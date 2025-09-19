09faeaca2492f4de27dd685eafaa8d1bdba474aa
Eukingzinn <thallesoares2@gmail.com>
2025-09-19 09:55:54 -0300

    19/09/2025

--- Curso HTML-CSS/backend/ex003/entrega/cad.php
+++ Curso HTML-CSS/backend/ex003/entrega/cad.php
@@ -12,21 +12,23 @@
     </header>
     <main>
         <?php 
-        $largura = $_POST["largura"];
-        $altura = $_POST["altura"];
-        $area = $largura * $altura;
-        $quantidade_de_tinta = $area / 2;
+        if (isset($_POST["largura"]) && isset($_POST["altura"])) {
+            $largura = floatval($_POST["largura"]);
+            $altura  = floatval($_POST["altura"]);
 
+            $area = $largura * $altura;
+            $quantidade_de_tinta = $area / 2;
 
-
-       echo "<p> Bem... de acordo com as seguintes informações: <br>" . 
-       "A sua parede possuí: $largura m de largura <br>" . 
-       "A sua parede possuí: $altura m de altura <br>".
-       "Sua parede ao todo, mede $area m² <br>". 
-       "Serão necessários $quantidade_de_tinta litros de tinta. </p>";
+            echo "<p> Bem... de acordo com as seguintes informações: <br>" . 
+            "A sua parede possui: $largura m de largura <br>" . 
+            "A sua parede possui: $altura m de altura <br>" .
+            "Sua parede ao todo, mede $area m² <br>". 
+            "Serão necessários $quantidade_de_tinta litros de tinta. </p>";
+        } else {
+            echo "<p>Erro: por favor preencha os valores da largura e altura no formulário.</p>";
+        }
         ?>
-<p><a href="javascript:history.go(-1)">Voltar para a página anterior</a></p>
-
+        <p><a href="javascript:history.go(-1)">Voltar para a página anterior</a></p>
     </main>
 </body>
 </html>
